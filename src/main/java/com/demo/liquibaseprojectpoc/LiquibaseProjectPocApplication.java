c1a11373642a9dafa89556adfed7d72aebcf3780
Anup Gupta <anup.gupta@nonstopio.com>
2025-04-10 18:36:33 +0530

    initialize Liquibase project with Spring Boot and MySQL configuration

---
+++ src/main/java/com/demo/liquibaseprojectpoc/LiquibaseProjectPocApplication.java
@@ -0,0 +1,13 @@
+package com.demo.liquibaseprojectpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseProjectPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseProjectPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/liquibaseprojectpoc/LiquibaseProjectPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.liquibaseprojectpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseProjectPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
